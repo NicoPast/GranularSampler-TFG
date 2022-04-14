1627f267faa81d5f3ba1cb60f310f6c91a5b0555
Nico <nicopast@ucm.es>
2022-04-15 00:17:54 +0200

    Tweaked button text when enabled or disabled

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -56,10 +56,10 @@ void LookAndFeel::drawRotarySlider(juce::Graphics& g, int x, int y,
         r.setSize(strWtidth + 4, rswl->getTextHeight() + 2);
         r.setCentre(bounds.getCentre());
 
-        g.setColour(Colours::black);
+        g.setColour(enabled ? Colours::black : Colours::darkgrey);
         g.fillRect(r);
 
-        g.setColour(Colours::white);
+        g.setColour(enabled ? Colours::white : Colours::lightgrey);
         g.drawFittedText(text, r.toNearestInt(), juce::Justification::centred, 1);
     }
 }
